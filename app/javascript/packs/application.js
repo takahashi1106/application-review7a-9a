2ee9a675d3006748bf908d0545056e5d96538ee6
takahashi1106 <tk2696274@gmail.com>
2022-04-06 10:44:08 +0000

    [Update]

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,7 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery
-//= require rails-ujs
+
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -14,6 +13,7 @@ import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application" 
+import "script.js"
 
 Rails.start()
 Turbolinks.start()
