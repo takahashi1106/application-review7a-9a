cd18e4dc1d6ef77f2c3aaf50506f23cee3ff1cad
takahashi1106 <tk2696274@gmail.com>
2022-04-07 06:39:20 +0000

    [Update]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,24 +1,30 @@
 class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
+
+
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.user_id = current_user.id
+    unless @book_comment.save
+      render 'error'
+    end
   end
-    #１行目：book = Book.find(params[:book_id])　
+
+    #１行目：book = Book.find(params[:book_id])
     #(params[:book_id])　で、本の投稿から”ある投稿”を取り出せます。
     #findメソッドを利用して、データベース　から取り出して、bookというローカル変数に入れてあります。
     #２行目　：コメントを新規投稿するための空のインスタンスを作っています。
-    #３行目：comment.book_id=book.id　　
+    #３行目：comment.book_id=book.id
     #空のインスタンスのbook_idは、今取り出してる"ある投稿”のidだよという意味だと思います。
     #”ある投稿”に対してコメントしますよ〜という感じ
 
   def destroy
-    BookComment.find(params[:id]).destroy
-     redirect_back(fallback_location: root_path)
+    @book=Book.find(params[:book_id])
+    book_comment=@book.book_comments.find(params[:id])
+    book_comment.destroy
   end
-
 
 
   private
@@ -26,4 +32,5 @@ class BookCommentsController < ApplicationController
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
+
 end
